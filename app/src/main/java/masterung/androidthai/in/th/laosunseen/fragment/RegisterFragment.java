a91ef2c3a752f770cb699be058c923fea5a90d7c
douangsamone <samoneborh@gmail.com>
2018-08-07 15:54:50 +0700

    update7Aug18

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
@@ -1,6 +1,9 @@
 package masterung.androidthai.in.th.laosunseen.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,17 +11,49 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import masterung.androidthai.in.th.laosunseen.MainActivity;
 import masterung.androidthai.in.th.laosunseen.R;
 
 public class RegisterFragment extends Fragment {
 
+//    Explicit
+    private Uri uri;
+    private ImageView imageView;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
 //        create Toolbar
+        createToolbar();
+
+//        Photo Controller
+        photoController();
+
+    }//Main class
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void photoController() {
+        imageView = getView().findViewById(R.id.imvPhoto);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // invoke picture
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");// any software to open photo
+                startActivityForResult(Intent.createChooser(intent,"Please Choose app"),1);
+            }
+        });
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register"); //set title on toolbar
@@ -35,8 +70,7 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack(); // press arrow navigator and back to Home
             }
         });
-
-    }//Main class
+    }
 
     @Nullable
     @Override
