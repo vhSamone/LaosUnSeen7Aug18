2695adef339c9b044040b3509afcd93b04e70e58
douangsamone <samoneborh@gmail.com>
2018-08-07 15:09:51 +0700

    ToolBar Success

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/MainFragment.java
@@ -5,12 +5,37 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import masterung.androidthai.in.th.laosunseen.R;
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//      Register
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+    }//Method Main
+
 
     @Nullable
     @Override
@@ -19,3 +44,4 @@ public class MainFragment extends Fragment{
         return view;
     }
 }
+
