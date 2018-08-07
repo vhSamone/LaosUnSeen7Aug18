041d64898ae7180f3edcc97c0ccc4885c95bf1e7
douangsamone <samoneborh@gmail.com>
2018-08-07 17:05:36 +0700

    finished7Aug

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
@@ -1,6 +1,8 @@
 package masterung.androidthai.in.th.laosunseen.fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -9,16 +11,20 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import masterung.androidthai.in.th.laosunseen.MainActivity;
 import masterung.androidthai.in.th.laosunseen.R;
 
 public class RegisterFragment extends Fragment {
 
-//    Explicit
+    //    Explicit
     private Uri uri;
     private ImageView imageView;
 
@@ -35,8 +41,38 @@ public class RegisterFragment extends Fragment {
     }//Main class
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 400, 300, true);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getActivity(), "Please Choose Photo", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void photoController() {
@@ -48,14 +84,14 @@ public class RegisterFragment extends Fragment {
                 // invoke picture
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");// any software to open photo
-                startActivityForResult(Intent.createChooser(intent,"Please Choose app"),1);
+                startActivityForResult(Intent.createChooser(intent, "Please Choose app"), 1);
             }
         });
     }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register"); //set title on toolbar
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Should Photo and fill All Blank");
 
@@ -70,6 +106,7 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack(); // press arrow navigator and back to Home
             }
         });
+        setHasOptionsMenu(true);
     }
 
     @Nullable
